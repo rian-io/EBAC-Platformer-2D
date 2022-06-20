40b06b6ddad1d3d6de0cbd5374ac9f70518f514e
Rian I. de Oliveira <rian-io@users.noreply.github.com>
2022-06-19 22:48:06 -0300

    feat: Add player spawn animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GameManager : Singleton<GameManager>
 {
 
-  [Header("Player")]
-  public GameObject Player;
+    [Header("Player")]
+    public GameObject PlayerPrefab;
 
-  [Header("Enemies")]
-  public List<GameObject> Enemies;
+    [Header("Enemies")]
+    public List<GameObject> Enemies;
 
+    [Header("References")]
+    [SerializeField] private Transform StartPoint;
+
+    [Header("Animation")]
+    [SerializeField] private float _duration = 0.5f;
+    [SerializeField] private float _delay = 0.1f;
+    [SerializeField] private Ease _ease = Ease.OutBack;
+
+    private GameObject _currentPlayer;
+
+    private void Start() {
+        SpawnPlayer();   
+    }
+
+    private void SpawnPlayer() {
+        _currentPlayer = Instantiate(PlayerPrefab, StartPoint.transform);
+        _currentPlayer.transform.DOScale(Vector3.zero, _duration).From().SetDelay(_delay).SetEase(_ease);
+    }
 }
